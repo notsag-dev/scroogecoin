792307540581581efeb4fc00a3f995a57cd82572
Tonga <a.gaston.gimenez@gmail.com>
2017-09-29 00:43:36 -0300

    Add a method to get the hash of an object

--- hashutils.py
+++ hashutils.py
@@ -1,6 +1,12 @@
 import hashlib
 
-def hash_sha256(obj_str):
+def hash_sha256(encoded_str):
     hashfunc = hashlib.sha256()
-    hashfunc.update(obj_str)
+    hashfunc.update(encoded_str)
     return hashfunc.hexdigest()
+
+def hash_object(obj):
+    """ Return the sha256 hash of the encoded string of
+        the object passed as parameter.
+    """
+    return hash_sha256(str(obj).encode('utf-8'))
